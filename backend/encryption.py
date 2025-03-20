4e20748fa680d7487e6fbd6fb639292def0a4671
Emma Murphy <murphe80@tcd.ie>
2025-03-20 12:57:15 +0000

    integrated google drive API

---
+++ backend/encryption.py
@@ -0,0 +1,118 @@
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+import os 
+
+
+def encrypt_file(input_file, output_file):
+    """ 
+    Encrypts a given file using AES 256 encryption
+
+    Args: 
+        input_file (String) : file to be encrypted 
+        output_file (String) : file for encrypted file to be written to 
+    Returns: 
+        key (int) : 32 bit key for decryption 
+        iv (int) : 16 bit iv for decryption
+       
+    """
+
+    key = os.urandom(32)
+    iv = os.urandom(16)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    try:
+        #read file in binary mode 
+        with open(input_file, "rb") as f: 
+            plaintext = f.read()
+
+        #pad contents to make multiple of 16 bytes 
+        pad_plaintext = pad(plaintext)
+        ciphertext = cipher.encrypt(pad_plaintext)
+
+        with open(output_file, "wb") as f:
+            f.write(ciphertext)
+
+        # encoded_key = key.hex()
+        encoded_iv = iv.hex()
+
+        encrypted_key = encryptAES(key).hex()
+
+        return True
+
+    except Exception as e:
+        print(f"An error occured opening the file: {e}")
+        return False
+
+def pad(data):
+    """Pads data to be a multiple of 16 bytes (AES block size)."""
+    pad_length = 16 - len(data) % 16
+    return data + bytes([pad_length] * pad_length)
+
+def decrypt_file(input_file, output_file, key, iv):
+    """
+
+        Decrypts file using key and iv 
+
+        Args:
+            input_file (string) : file to be decrypted 
+            output_file (string) : file where plaintext is to be written to
+            key (hex) : key to be used for decryption 
+            iv (hex) : iv to be used for decryption
+
+    """
+    try:
+        
+        #decrypt aes key
+        decrypted_aes = decryptAES(key)
+
+        # decoded_key = bytes.fromhex(key)
+        decoded_iv = bytes.fromhex(iv)
+
+        if len(decrypted_aes) != 32:
+            print("Incorrect length for AES key")
+            return False
+
+        cipher = AES.new(decrypted_aes, AES.MODE_CBC, decoded_iv)
+
+        with open(input_file, "rb") as f:
+            ciphertext = f.read()
+
+        #unpad data
+        plaintext = unpad(cipher.decrypt(ciphertext))
+
+        #write to output file 
+        with open(output_file, "wb") as f:
+            f.write(plaintext)
+        
+        print("file decrypted successfully")
+        return True
+        
+    except Exception as e:
+        print(f"An error occured decrypting the file: {e}")
+        return False
+        
+def unpad(data):
+    """unpads data from AES block size"""
+    pad_length = data[-1]
+    return data[:-pad_length]
+
+
+def encryptAES(aes_key):
+    try:
+        # retrieve RSA public key 
+        with open("public.pem", "rb") as f:
+            public_key = RSA.import_key(f.read())
+        cipher_rsa = PKCS1_OAEP.new(public_key)
+        return cipher_rsa.encrypt(aes_key)
+    
+    except Exception as e:
+        print(f"error encrypting AES key : {e}")
+
+def decryptAES(aes_key):
+    try:
+        #retrieve RSA private key
+        with open("private.pem", "rb") as f:
+            private_key = RSA.import_key(f.read())
+        cipher_rsa = PKCS1_OAEP.new(private_key)
+        return cipher_rsa.decrypt(aes_key)
+    except Exception as e:
+        print(f"error decrypting AES key : {e}")
